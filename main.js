e7262df6f7b2b87b9c4e9cc9ddd08cd8c27420db
oliveirapaulalaura-cmd <oliveira.paula.laura@escola.pr.gov.br>
2026-08-06 22:42:53 -0300

    Aula06/08

---
+++ main.js
@@ -0,0 +1,12 @@
+const boyos = document.querySelectorAll("butto");
+
+botoes.for Each = false;
+botao.addEventListener("click", botaoClicado);
+function botaoClicado(){
+    let texto = botao.queySelector("span");
+if (curtiu ===false) {
+    texto.textContent++;
+    curtiu= true;
+}
+}
+    
